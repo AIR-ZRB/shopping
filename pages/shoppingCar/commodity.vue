9a3da9db2a494bac607e993bce92ec2540c342a4
青空 <1057605824@qq.com>
2020-09-04 10:28:19 +0800

    修改图标，购物车逻辑

--- pages/shoppingCar/commodity.vue
+++ pages/shoppingCar/commodity.vue
@@ -1,30 +1,33 @@
 <template>
 	<view class="commodity">
+		<!-- 商品名字 -->
 		<view class="store-name">
-			<label class="radio">
-				<radio value="r1" checked="true" />
-				<text class="name">{{_props.items.storeName}}</text>
-			</label>
-
-
+			<radio-group @change="radioChange">
+				<label class="radio">
+					<radio :value="_props.items.storeName" :checked="storeSelect" />
+					<text class="name">{{_props.items.storeName}}</text>
+				</label>
+			</radio-group>
 		</view>
 
-		<view class="detail-message">
+
+		<view class="detail-message" v-for="items in _props.items.children" :key="items.name+ items.price">
+			<!-- 商品图片 -->
 			<label class="radio">
-				<radio value="" checked="true" />
+				<radio value="" :checked="items.check" />
+				<view class="image"></view>
 			</label>
 
-			<view class="image"></view>
-
+			<!-- 商品信息 -->
 			<view class="commodity-description">
-				<text class="name">{{_props.items.name}}</text>
-				<text class="select">{{_props.items.select}}</text>
+				<text class="name">{{items.name}}</text>
+				<text class="select">{{items.select}}</text>
 
 				<view class="price-count">
-					<text class="price">￥{{_props.items.price}}</text>
+					<text class="price">￥{{items.price}}</text>
 					<view class="count">
 						<text>-</text>
-						<text>{{_props.items.count}}</text>
+						<text>{{items.count}}</text>
 						<text>+</text>
 					</view>
 				</view>
@@ -37,6 +40,8 @@
 			</view>
 
 
+
+
 		</view>
 
 
@@ -45,12 +50,35 @@
 
 <script>
 	export default {
+		props: ["items"],
 		data() {
 			return {
-
+				storeSelect: false
 			}
 		},
-		props: ["items"]
+		methods: {
+			radioChange(value) {
+				// let datas = JSON.parse(JSON.stringify(this._props.items));
+				this.storeSelect = !this.storeSelect;
+				
+				// console.log(this.storeSelect);
+				
+				// let datas = this._props.items;
+				// datas.children.forEach((item) => item.check = !item.check)
+				// this.$emit("update:items", datas);
+
+
+
+				// console.log(datas)
+				// this.$nextTick(function() {
+				// 	console.log(this._props.items)
+				// })
+
+			}
+
+		}
+
+
 	}
 </script>
 
@@ -68,37 +96,51 @@
 		}
 
 		.detail-message {
-			position: relative;
-			@include flex-layout(flex-start,center);
+			@include flex-layout(flex-start);
+			padding-bottom: 20px;
 
-			.image {
-				width: 135px;
-				height: 135px;
-				background-color: red;
-				margin:0 0 0 10px;
+			.radio {
+				@include flex-layout(center, center);
+				float: left;
+				position: absolute;
+
+
+				.image {
+					width: 135rpx;
+					height: 135rpx;
+					background-color: red;
+					margin: 0 0 0 10px;
+				}
 			}
 
+
+
+			// 商品详细信息
 			.commodity-description {
 				box-sizing: border-box;
+				overflow: hidden;
+				margin-left: 220rpx;
+
 				width: 100%;
-				position: absolute;
-				top: 0;
-				padding-left: 190px;
+				position: relative;
+
+
 
 				.name {
 					display: block;
 					// font-size: 18px;
-					font-size: 32rpx;
+					font-size: 30rpx;
 				}
 
 				.select {
 					// font-size: 14px;
+					display: inline-block;
 					font-size: 28rpx;
 					$select-height: 24px;
 					height: $select-height;
 					border-radius: $select-height / 2;
 					background-color: #F2F2F2;
-					padding: 0 2px;
+					padding: 0 4px;
 					margin: 10px 0;
 				}
 
--- pages/shoppingCar/shoppingCar.vue
+++ pages/shoppingCar/shoppingCar.vue
@@ -1,12 +1,8 @@
 <template>
 	<view class="shopping-car">
 		<clearCar v-if="commodity.length === 0" />
-		<commodity v-else
-			v-for="item in commodity"
-			:key="item.name + item.storeName"
-			:items= "item"
-		/>
-		
+		<commodity v-else v-for="item in commodity" :key="item.name + item.storeName" :items.sync="item" />
+
 	</view>
 </template>
 
@@ -17,26 +13,36 @@
 		data() {
 			return {
 				// 假数据
-				commodity: [
-					{
-						name: "PS4",
-						picture: "",
-						price: "4000",
-						select: "标准普通版",
-						check: false,
-						count: "1",
+				commodity: [{
 						storeName: "青空自营",
+						children: [{
+							name: "PS4 超强游戏机，各大平台游戏主机",
+							picture: "",
+							price: "4000",
+							select: "标准普通版",
+							check: false,
+							count: "1",
+						}, {
+							name: "红米K30至尊版 超强技能手机，性价比之王",
+							picture: "",
+							price: "2000",
+							select: "标准普通版",
+							check: false,
+							count: "1",
+						}]
 					},
 					{
-						name: "红米K30至尊版",
-						picture: "",
-						price: "2000",
-						select: "标准普通版",
-						check: false,
-						count: "1",
-						storeName: "青空自营",
+						storeName: "PDD旗舰店",
+						children: [{
+							name: "Redmi 9",
+							picture: "",
+							price: "1000",
+							select: "标准普通版",
+							check: false,
+							count: "1",
+						}],
 					},
-					
+
 				]
 			};
 		},
@@ -48,7 +54,7 @@
 </script>
 
 <style lang="scss">
-.shopping-car {
-	background-color: #fafafa;
-}
+	.shopping-car {
+		background-color: #fafafa;
+	}
 </style>
