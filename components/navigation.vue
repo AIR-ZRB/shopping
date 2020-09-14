f1990d2eab1d865b27a1e3dab2079059882f1a71
青空 <1057605824@qq.com>
2020-09-14 11:18:09 +0800

    完成navigation组件

---
+++ components/navigation.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="navigation">
+		<image src="../static/icon/left-arrow.png"></image>
+		<slot></slot>
+		<image src="../static/icon/else.png"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.navigation {
+	box-sizing: border-box;
+	width: 100%;
+	height: 45px;
+	border-bottom: 1px solid #eee;
+	padding: 0 10px;
+	@include flex-layout(space-between,center);
+	font-size: 18px;
+	image {
+		height: 20px;
+		width: 20px;
+	}
+}
+</style>
--- pages/classify/classify.vue
+++ pages/classify/classify.vue
@@ -1,7 +1,21 @@
 <template>
+	<view>
+		<un-navigation>
+			<text>分类</text>
+		</un-navigation>
+	</view>
 </template>
 
 <script>
+	import unNavigation from "../../components/navigation.vue";
+	export default {
+		data() {
+			return {}
+		},
+		components: {
+			unNavigation
+		}
+	}
 </script>
 
 <style>
