b0464509dce1d89252bfa425346826218c65b75b
AIR-ZRB <1057605824@qq.com>
2020-12-17 10:48:42 +0800

    完善商品详情

--- pages/product/product.vue
+++ pages/product/product.vue
@@ -1,5 +1,8 @@
 <template>
 	<view class="product">
+		<view class="product-image">
+			<image :src="product.image"></image>
+		</view>
 		<view class="product-message">
 			<text class="price">￥{{product.price}}</text>
 			<view>
@@ -9,18 +12,23 @@
 		</view>
 
 		<view class="discounts">
-			<text>优惠</text>
+			<h3>优惠</h3>
 			<text>体验卡免费领，超大流量任性用</text>
 		</view>
 
 		<view class="delivery">
 			<view>
-				<text>已选</text>
+				<h3 class="">已选</h3>
+				<text>小米手表color 雅典黑，新款手表，1个</text>
 			</view>
 			<view>
-				<text>送至</text>
+				<h3>送至</h3>
+				<text>北京朝阳区三环到四环之间</text>
 			</view>
-			<view><text>运费</text></view>
+			<view>
+				<h3>运费</h3>
+				<text>在线支付免运费</text>
+			</view>
 		</view>
 
 	</view>
@@ -33,7 +41,7 @@
 				product: {
 					price: "699",
 					title: "小米（MI） 小米手表color高清大屏智能手表长续航NFC支付多功能防水手表 小米手表color 典雅黑 新款手表",
-
+					image: "//m.360buyimg.com/mobilecms/s750x750_jfs/t1/120709/10/10042/182270/5f3b84dcE996bcdca/b99a95682a13b63f.jpg!q80.dpg.webp"
 				}
 			}
 		},
@@ -49,9 +57,17 @@
 		height: 100%;
 	}
 
+	.product-image {
+		image {
+			width: 100%;
+			height: 375px;
+		}
+	}
+
 	.product-message {
 		background-color: #FFFFFF;
 		padding: 20px;
+		border-radius: 10px;
 
 		.price {
 			font-size: 24px;
@@ -74,19 +90,30 @@
 	}
 
 	.discounts {
-		font-size: 14px;
-		font-weight: 700;
+		
 		margin: 20px 0;
 		padding: 10px 20px;
 		background: #FFFFFF;
+		border-radius: 10px;
+		@include flex-layout(flex-start,center)
+		text {
+			margin-left: 20px;
+			font-size: 12px;
+		}
 	}
 
 	.delivery {
 		background: #FFFFFF;
-		padding: 10px 20px;
+		padding: 0 20px;
+		border-radius: 10px;
+
 		view {
-			padding: 10px 0;
-			font-weight: 700;
+			padding: 20px 0;
+			@include flex-layout(flex-start,center);
+			text {
+				font-size: 12px;
+				margin-left: 20px;
+			}
 		}
 	}
 </style>
